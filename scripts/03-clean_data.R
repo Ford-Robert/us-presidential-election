30c286c15832e6c133bd1f5aba19944023d0ea8c
Robert Ford <robert.ford@mail.utoronto.ca>
2024-10-13 22:35:51 -0400

    added days_to_election, and new cleaned_poll_data

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -14,8 +14,6 @@ library(lubridate)
 # read file first
 raw_poll_data <- read_csv("data/raw_poll_data.csv")
 
-election_date <- 11-5-24
-
 # filter out polls from before Biden dropped out of race
 raw_poll_data <- raw_poll_data |>
   mutate(end_date = mdy(end_date)) 
@@ -39,8 +37,20 @@ cleaned_poll_data <- cleaned_poll_data %>%
 
 cleaned_poll_data <- na.omit(cleaned_poll_data)
 
-View(cleaned_poll_data)
+# Adding how many days before the election that the poll was conducted
 
+election_date <- 11-5-24
+cleaned_poll_data <- cleaned_poll_data %>%
+  mutate(
+    election_date = as.Date("11-5-24", format = "%m-%d-%y")
+  )
+
+# Calculate the days to election and add as a new column
+cleaned_poll_data <- cleaned_poll_data %>%
+  mutate(days_to_election = as.numeric(election_date - end_date))
+
+View(cleaned_poll_data)
+str(cleaned_poll_data)
 # write to csv
 write_csv(cleaned_poll_data, "data/cleaned_poll_data.csv")
 
