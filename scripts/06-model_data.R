ee981c4a4b06c46fdf9b4a3f0fe0e7356c8cca3e
Robert Ford <robert.ford@mail.utoronto.ca>
2024-10-14 15:05:23 -0400

    fixed

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -1,12 +1,10 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models the regional support, then calculates the EV each canditate is
+#          expected to get
+# Author:  Robert Ford, Michelle Ji
+# Date: 14 October 2024
+# Contact: robert.ford@mail.utoronto.ca, michelle.ji@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
 library(tidyverse)
