306764ecf92067c64b3ef3fa78433c64fc1093a6
michj3 <michelle.ji@mail.utoronto.ca>
2024-10-19 17:25:20 -0400

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -22,9 +22,12 @@ states <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado"
               "Rhode Island", "South Carolina", "South Dakota", "Tennessee", 
               "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", 
               "Wisconsin", "Wyoming")
-pollsters <- c(unique(cleaned_poll_data$`Pollster Name`))
-collection_methods <- c(unique(cleaned_poll_data$Methodology))
-candidate <- c(unique(cleaned_poll_data$`Candidate Favored`))
+pollsters <- c(unique(cleaned_poll_data$pollster))
+collection_methods <- c(unique(cleaned_poll_data$method))
+candidate_favored <- c(unique(cleaned_poll_data$candidate))
+start_date <- as.Date("2024-07-22")
+end_date <- as.Date("2024-10-08")
+date_sequence <- seq.Date(from = start_date, to = end_date, by = "day")
 
 # simulate
 set.seed(3)
@@ -32,9 +35,17 @@ simulated_poll_data <-
   tibble(
     "Pollster Name" = sample(pollsters, size = 100, replace = TRUE),
     "Methodology" = sample(collection_methods, size = 100, replace = TRUE),
+    "Numeric Grade" = sample(0.7:3.0, size = 100, replace = TRUE),
+    "Poll Score" = sample(-1.5:1.7, size = 100, replace = TRUE),
+    "Date Ended" = sample(date_sequence, size = 100, replace = TRUE),
+    "Transparency Score" = sample(1:10, size = 100, replace = TRUE),
+    "Question ID" = sample(204124:212159, size = 100, replace = TRUE),
+    "Support" = sample(0:70, size = 100, replace = TRUE),
+    "Election Date" = "2024-11-05",
+    "Days to Election" = sample(28:105, size = 100, replace = TRUE),
     "State" = sample(states, size = 100, replace = TRUE),
     "Type of Voter" = sample(c("lv", "rv", "a", "v"), size = 100, replace = TRUE),
-    "Candidate Favored" = sample(candidate, size = 100, replace = TRUE),
+    "Candidate Favored" = sample(candidate_favored, size = 100, replace = TRUE),
     "Percentage" = sample(1:100, size = 100, replace = TRUE)
   )
 
