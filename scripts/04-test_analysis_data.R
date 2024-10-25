4e210e0741b3c021beaf885d940946e65e0da7cc
michj3 <michelle.ji@mail.utoronto.ca>
2024-10-25 13:38:54 -0400

    Update 04-test_analysis_data.R

--- scripts/04-test_analysis_data.R
+++ scripts/04-test_analysis_data.R
@@ -25,3 +25,49 @@ clean_data$`Candidate Favored` |> class() == "character"
 max(cleaned_poll_data$transparency_score) == 10
 min(cleaned_poll_data$transparency_score) == 1
 
+
+# Test historical wins
+
+historical_wins <- tibble(
+  State = c("California", "Massachusetts", "Texas", "West Virigina"),
+  Party_Lean = c("Democrat", "Democrat", "Republican", "Republican")
+)
+
+
+
+test_that("State wins align with historical trends", {
+  # Filter Kamala Harris results
+  harris_results <- cleaned_poll_data |>
+    filter(cleaned_poll_data$candidate == "Harris")
+  
+  
+  historical_dem_states <- historical_wins %>%
+    filter(Party_Lean == "Democrat") %>%
+    pull(State)
+  
+  for (state in historical_dem_states) {
+    avg_support <- harris_results %>%
+      filter(state == state) %>%
+      summarize(avg_support = mean(support, na.rm = TRUE)) %>%
+      pull(avg_support)
+    
+    
+    
+    expect_true(avg_support > 50, info = paste("Kamala Harris is winning", state))
+  }
+  
+  # Check that she does not win in historically Republican states
+  historical_rep_states <- historical_wins %>%
+    filter(Party_Lean == "Republican") %>%
+    pull(State)
+  
+  for (state in historical_rep_states) {
+    avg_support <- harris_results %>%
+      filter(state == state) %>%
+      summarize(avg_support = mean(support, na.rm = TRUE)) %>%
+      pull(avg_support)
+    
+    # Test if average support in these states is less than 50%
+    expect_true(avg_support <= 50, info = paste("Kamala Harris is losing", state))
+  }
+})
